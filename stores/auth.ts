725115803a09421864f7efdf02897ce480bb7c75
chelleonis <roflcopterx1@gmail.com>
2024-11-15 14:25:21 -0500

    temp progress

--- stores/auth.ts
+++ stores/auth.ts
@@ -2,5 +2,11 @@ export const useAuthStore = defineStore('auth', {
     state: () => ({
         token: null as string | null,
         user: null as any
-    })
+    }),
+
+    actions: {
+        async login(username: string, password: string) {
+            
+        }
+    }
 })
