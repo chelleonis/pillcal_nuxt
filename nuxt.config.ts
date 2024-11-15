a430a604eb97c4a37ae799ffecc072de3a978fda
chelleonis <roflcopterx1@gmail.com>
2024-11-15 13:48:53 -0500

    adding tailwind and primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    port: 3030
+  },
+  modules: [
+    '@primevue/nuxt-module'
+],
+css: ['~/assets/css/main.css'],
+primevue: {
+},
+postcss: {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+},
 })
